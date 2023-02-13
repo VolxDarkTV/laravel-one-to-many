8338262b5fe421f5f20a64c7bfacce4753695beb
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-13 16:50:39 +0100

    add a fiew of layout

--- laravel-foreign-key/resources/views/pages/home.blade.php
+++ laravel-foreign-key/resources/views/pages/home.blade.php
@@ -8,37 +8,54 @@
     <h1>Home</h1>
 
     <section>
+
         <div class="container">
-            
+
             @foreach ($people as $person)
-                <div class="card">
-                    <h6>Name: {{$person -> name}}</h6>
-                    <div>
-                        <span>
-                            {{$person -> personDetail -> phone}}
-                        </span>
-                    </div>
-                    <div>
-                        <span>
-                            {{$person -> mail}}
-                        </span>
-                    </div>
+                <div class="d-flex flex-column gap-2 py-5">
 
-                    <div class="p-5">
-                        <h5>Post</h5>
-                        @foreach ($person -> posts as $post)
-                            <div>
-                                <h5 class="fw-bold">{{$post -> title}}</h5>
+                    <div class="card pt-1">
+                        {{-- Profile name --}}
+                        <div class="d-flex flex-column align-items-center gap-2">
+                            <h6 class="bg-success rounded-pill p-2">{{$person -> name}}</h6>
+                        </div>
+    
+                        <div class="p-5">
+                            <h5>Post</h5>
+                            @foreach ($person -> posts as $post)
+                                <div>
+                                    <h5 class="fw-bold">{{$post -> title}}</h5>
+    
+                                    <p>
+                                        {{$post -> body}}
+                                    </p>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-around">
 
-                                <p>
-                                    {{$post -> body}}
-                                </p>
-                            </div>
-                        @endforeach
+                        {{-- Profile Number --}}
+                        <div>
+                            <span>Phone </span>
+                            <span class="bg-warning rounded-pill p-2">
+                               {{$person -> personDetail -> phone}}
+                            </span>
+                        </div>
+
+                         {{-- Profile mail --}}
+                         <div>
+                            <span>Mail</span>
+                            <span class="bg-danger p-2 rounded-pill">
+                                {{$person -> mail}}
+                            </span>
+                        </div>
+
                     </div>
                 </div>
             @endforeach
 
         </div>
+
     </section>
 @endsection
