be9b0f00368e815b616b7aecc0e93d549a6acbee
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-13 15:27:16 +0100

    add model

--- laravel-foreign-key/database/migrations/9999_02_13_111248_add_foreign_keys.php
+++ laravel-foreign-key/database/migrations/9999_02_13_111248_add_foreign_keys.php
@@ -44,6 +44,16 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('posts', function (Blueprint $table) {
+            
+            $table->dropForeign('posts_person_id_foreign');
+            $table->dropColumn('person_id');
+        });
+
+        Schema::table('person_details', function (Blueprint $table) {
+            
+            $table->dropForeign('person_details_person_id_foreign');
+            $table->dropColumn('person_id');
+        });
     }
 };
